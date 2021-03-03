089126d8c1d91cc6836a60abecdba2dbc339f46d
Matias Borghi <borghi.matias@gmail.com>
2021-03-02 23:24:52 -0300

    minor changes + add gh-pages configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,13 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Matias Borghi',
     description:
-      'Brittany Chiang is a software engineer specializing in building (and occasionally designing) exceptional websites, applications, and everything in between.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Matias Borghi is a software engineer specializing in building (and occasionally designing) exceptional websites, applications, and everything in between.',
+    siteUrl: 'https://mattborghi.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@borghim',
+    pathPrefix: "/v4", // used for gh-pages and gatsby build
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -19,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'MatiasBorghi',
+        short_name: 'MatiasBorghi',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
